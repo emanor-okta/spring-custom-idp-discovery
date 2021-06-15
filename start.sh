ee253421ef76af2e434fc83ac51215dad3ecc2d8
Erik Manor <erik.manor@okta.com>
2021-06-15 15:22:29 -0700

    initial

---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mvn spring-boot:run -Dokta.oauth2.clientId=0oa8rc1vvUWbpGroC1d6 -Dokta.oauth2.clientSecret=-pEqOYnbPyvke0jxgaUYOYmQplYI4DJ7VzKMiu96 -Dokta.oauth2.issuer=https://okta.oktamanor.com/oauth2/default 
+
